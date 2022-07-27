dbb84f2ccf9d62f633afb4fa1b186f337857eb38
Holly McGuinness <gaithelema@gmail.com>
2022-07-27 12:33:19 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', function(req, res) {
+    // the following makes index.html run on a server
+    res.sendFile(path.join(__dirname + '../index.html'))
+})
+
+// gets the port from heroku, or uses 4005
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
+
+
+
